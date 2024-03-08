1aa3e21789a1d9333bbfd7d2591a2a947df2d4f4
HoussemDAAS <houssemdaas2@gmail.com>
2024-03-08 18:04:26 +0100

    haandeling error and loading using States and try catch

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,9 +1,11 @@
-import Places from './Places.jsx';
-import { useEffect, useState } from 'react';
-
+import Places from "./Places.jsx";
+import { useEffect, useState } from "react";
+import Error from "./Error.jsx";
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
-// exp1
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  // exp1
   // useEffect(()=>{
   //   fetch('http://localhost:3000/places').then((response) => {
   //     return response.json();
@@ -13,20 +15,35 @@ export default function AvailablePlaces({ onSelectPlace }) {
   // },[])
 
   // exp2
-  useEffect(() => {
- const fetchAvailablePlaces = async () => {
-   const response = await fetch('http://localhost:3000/places');
-   const data = await response.json();
-   setAvailablePlaces(data.places);
- }
 
- fetchAvailablePlaces();
+  useEffect(() => {
+    const fetchAvailablePlaces = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/placess");
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error(data.message || "Something went wrong!");
+        }
+        setAvailablePlaces(data.places);
+      } catch (error) {
+        setError({message: error.message||'Something went wrong!'});
+      }
+      setIsLoading(false);
+    };
+
+    fetchAvailablePlaces();
   }, []);
+  if(error){
+    return <Error title="An error occured" message={error.message} />
+  }
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      loadingText="Loading places..."
+      isLoading={isLoading}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
