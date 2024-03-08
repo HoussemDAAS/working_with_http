e8f7c44010985de2fd55634ee490485c0d130e22
HoussemDAAS <houssemdaas2@gmail.com>
2024-03-08 13:52:36 +0100

    using async await as a solution to fetch data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,14 +3,25 @@ import { useEffect, useState } from 'react';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+// exp1
+  // useEffect(()=>{
+  //   fetch('http://localhost:3000/places').then((response) => {
+  //     return response.json();
+  //   }).then((data) => {
+  //     setAvailablePlaces(data.places);
+  //   })
+  // },[])
 
-  useEffect(()=>{
-    fetch('http://localhost:3000/places').then((response) => {
-      return response.json();
-    }).then((data) => {
-      setAvailablePlaces(data.places);
-    })
-  },[])
+  // exp2
+  useEffect(() => {
+ const fetchAvailablePlaces = async () => {
+   const response = await fetch('http://localhost:3000/places');
+   const data = await response.json();
+   setAvailablePlaces(data.places);
+ }
+
+ fetchAvailablePlaces();
+  }, []);
 
   return (
     <Places
